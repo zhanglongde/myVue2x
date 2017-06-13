ecc886cea41200270f90622da0b8a5e1fbf9ab25
zhanglongde <1039326495@qq.com>
2017-06-13 18:51:46 +0800

    异步组件载入时清空状态

---
+++ src/components/exe/AddCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="addCard">创建</button>
+    <component :showHide="receiveAddData" :is="addModal"></component>
+  </div>
+</template>
+
+<script>
+const modal = resolve => require(['./Modal.vue'], resolve)
+export default {
+  name: 'addCard',
+  data () {
+    return {
+      addModal: null
+    }
+  },
+  methods: {
+    // 显示输入框
+    addCard () {
+      this.addModal = modal
+    },
+    receiveAddData (data) {
+       switch (data) {
+         case 1:
+             this.modifyTipSHow = true
+           this.modifyTip = '添加成功'
+             break;
+         case 2:
+           this.modifyTipShow = true
+           this.modifyTip = '添加失败, 请重试！'
+             break;
+       }
+       this.addModal = null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/exe/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal-box">
+    <div class="mask"></div>
+    <div class="container">
+      <div class="row">
+        <label>请输入开户行:</label><input type="text" v-model="account">
+      </div>
+      <div class="row">
+        <label>账户名称:</label><input type="text">
+      </div>
+      <div class="row">
+        <label>银行账号:</label><input type="text">
+      </div>
+      <div class="row">
+        <label>期初余额:</label><input type="text"><span>元</span>
+      </div>
+      <div class="row">
+        <button @click="showHide(1)">提交</button>
+        <button @click="save">保存</button>
+        <button>返回</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  data () {
+    return {
+      account: ''
+    }
+  },
+  props: [
+    'showHide'
+  ],
+  methods: {
+    save() {
+      console.log(this.account)
+    }
+  }
+}
+</script>
